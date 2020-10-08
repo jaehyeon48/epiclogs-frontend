311e44c6531469ac93e62fb33aa27fd02869d468
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-08 21:14:30 +0900

    Added processing google callback component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
 
+import GoogleCallback from './components/GoogleCallback';
+
 function App() {
   return (
     <Router>
       <Switch>
-        <div className="abc">가나다 abc 123</div>
+        <Route path="/oauth2/google-callback" component={GoogleCallback} exact />
       </Switch>
     </Router>
   );
