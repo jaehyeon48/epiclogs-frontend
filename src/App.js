f1429a56ebf2737daed0dbfebf4bca8ef4299ef1
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-08 10:39:32 +0900

    added fonts and default scss setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <div className="abc">가나다 abc 123</div>
       </Switch>
     </Router>
   );
