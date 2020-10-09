1d472b04f68e0b9fd88d2cccfeec6ba1e5b7e92d
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-09 09:52:07 +0900

    added login page's header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
 
+import Login from './components/auth/Login';
 import GoogleCallback from './components/GoogleCallback';
 import Navbar from './components/Navbar';
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
+        <Route path="/login" component={Login} exact />
         <Route path="/oauth2/google-callback" component={GoogleCallback} exact />
       </Switch>
     </Router>
---
+++ src/components/auth/Login.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Login = () => {
+  return (
+    <main className="login-page">
+      <header className="login-page__header">
+        <p className="login-page__site-name">EPICLOGS</p>
+        <h1 className="login-page__main-title">LOGIN</h1>
+      </header>
+    </main>
+  );
+}
+
+export default Login;
