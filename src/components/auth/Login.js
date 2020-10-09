60fb61051a3ab02b2f46ff3b10a58415e7a3074b
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-09 10:32:39 +0900

    added email and password inputs

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,12 +1,54 @@
 import React, { useState, useEffect } from 'react';
 
 const Login = () => {
+  const [loginForm, setLoginForm] = useState({});
+
+  const { email, password } = loginForm;
+
+  const handleChangeLoginForm = (e) => {
+    setLoginForm({ [e.target.name]: e.target.value });
+  }
+
+  const handleSubmitLoginForm = () => {
+
+  }
+
   return (
     <main className="login-page">
       <header className="login-page__header">
         <p className="login-page__site-name">EPICLOGS</p>
         <h1 className="login-page__main-title">LOGIN</h1>
       </header>
+      <section className="login-page__form-section">
+        <form
+          className="login-page__auth-form"
+          onSubmit={handleSubmitLoginForm}
+        >
+          <div className="login-page__email-form">
+            <input
+              id="login-email-input"
+              className="auth-input login-page__email"
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChangeLoginForm}
+            />
+            <label for="login-email-input">Email</label>
+          </div>
+          <div className="login-page__password-form">
+
+            <input
+              id="login-password-input"
+              className="auth-input login-page__password"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChangeLoginForm}
+            />
+            <label for="login-password-input">Password</label>
+          </div>
+        </form>
+      </section>
     </main>
   );
 }
