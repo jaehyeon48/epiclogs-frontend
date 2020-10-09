961e28b9cbb03ac480ebb315f8ea53c3cbbc1114
jaehyeon48 <imjaehyeon48@gmail.com>
2020-10-09 23:13:50 +0900

    set up redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.scss';
 
+import store from './store';
 import Login from './components/auth/Login';
 import GoogleCallback from './components/GoogleCallback';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/login" component={Login} exact />
-        <Route path="/oauth2/google-callback" component={GoogleCallback} exact />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/login" component={Login} exact />
+          <Route path="/oauth2/google-callback" component={GoogleCallback} exact />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
