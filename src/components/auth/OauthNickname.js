8934d54594fcf283693ebf30eca830001cd7339a
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-02 14:54:28 +0900

    checking query string

--- src/components/auth/OauthNickname.js
+++ src/components/auth/OauthNickname.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const OauthNickname = ({ location }) => {
-  console.log(location.search);
+  console.log(location.search.match(/(?!.*=).*/));
   return (
     <div>
-
+      oauth
     </div>
-  )
+  );
 }
 
 export default OauthNickname;
