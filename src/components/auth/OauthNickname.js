32e6dc61f8065c4688940fb7008e8b563b6098ad
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-02 18:51:55 +0900

    completed validating query string

--- src/components/auth/OauthNickname.js
+++ src/components/auth/OauthNickname.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import CryptoJS from 'crypto-js';
 require('dotenv').config();
 
 const OauthNickname = ({ location }) => {
-  if (location.search.length !== 47) { // including ?q=
-    return <Redirect to="/404" />
-  }
+  let history = useHistory();
+
+  useEffect(() => {
+    if (location.search.length !== 47) { // including ?q=
+      return history.push('/404');
+    }
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const passedUserId = location.search.slice(3);
+    (async () => {
+      const checkingRes = await axios.post(`${process.env.REACT_APP_SERVER_URL}/auth/check-google-user`,
+        JSON.stringify({ userId: passedUserId }), config);
+
+      if (checkingRes.data.res === -1) {
+        return history.push('/404');
+      }
+    })();
+  }, [location.search]);
 
   const decryptedUserId = CryptoJS.AES.decrypt(location.search.slice(3), process.env.REACT_APP_AES_SECRET);
 
