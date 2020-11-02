baf8cd6edf029e4aecce845b002c166cb16fc062
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-02 16:22:41 +0900

    parse query string using aes

--- src/components/auth/OauthNickname.js
+++ src/components/auth/OauthNickname.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
+import CryptoJS from 'crypto-js';
 
 const OauthNickname = ({ location }) => {
-  console.log(location.search.match(/(?!.*=).*/));
+  // console.log(location.search.match(/(?!.*=).*/));
+  console.log(CryptoJS.AES.decrypt(location.search.match(/(?!.*=).*/)[0]), 'a1mre43gsdfa9ger04gbq10fd9ae9bf9d');
   return (
     <div>
       oauth
