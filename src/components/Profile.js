1799bb0e7f613dc46d52af89d118221bef22fc11
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-03 18:38:59 +0900

    completed Profile page's style

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -12,9 +12,33 @@ import {
 
 const Profile = ({
   uploadAvatar,
-  deleteAvatar
+  deleteAvatar,
+  user
 }) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userNickname, setUserNickname] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [confirmPw, setConfirmPw] = useState('');
   const imageInputRef = useRef(null);
+
+  useEffect(() => {
+    if (user && user.email) {
+      setUserEmail(user.email);
+    }
+  }, [user, user.email]);
+
+  useEffect(() => {
+    if (user && user.name) {
+      setUserName(user.name);
+    }
+  }, [user, user.name]);
+
+  useEffect(() => {
+    if (user && user.nickname) {
+      setUserNickname(user.nickname);
+    }
+  }, [user, user.nickname]);
 
   const handleTriggerImageInput = () => {
     imageInputRef.current.click();
@@ -37,6 +61,26 @@ const Profile = ({
 
   const handleDeleteAvatar = () => {
     deleteAvatar();
+  }
+
+  const handleChangeUserEmail = (e) => {
+    setUserEmail(e.target.value);
+  }
+
+  const handleChangeUserName = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const handleChangeUserNickname = (e) => {
+    setUserNickname(e.target.value);
+  }
+
+  const handleChangeUserPassword = (e) => {
+    setUserPassword(e.target.value);
+  }
+
+  const handleChangeConfirmPw = (e) => {
+    setConfirmPw(e.target.value);
   }
 
   return (
@@ -65,11 +109,80 @@ const Profile = ({
           >Remove Image</button>
         </div>
       </div>
+      <div className="profile__user-info">
+        <div className="user-infos profile__user-email-container">
+          <label htmlFor="profile__user-email">Email</label>
+          <input
+            id="profile__user-email"
+            type="text"
+            name={userEmail}
+            value={userEmail}
+            onChange={handleChangeUserEmail}
+            disabled
+          />
+        </div>
+        <div className="user-infos profile__user-name-container">
+          <label htmlFor="profile__user-name">Name</label>
+          <input
+            id="profile__user-name"
+            type="text"
+            name={userName}
+            value={userName}
+            onChange={handleChangeUserName}
+          />
+          <button
+            type="button"
+            className="btn-modify-profile btn-modify-user-name"
+          >Modify</button>
+        </div>
+        <div className="user-infos profile__user-nickname-container">
+          <label htmlFor="profile__user-nickname">Nickname</label>
+          <input
+            id="profile__user-nickname"
+            type="text"
+            name={userNickname}
+            value={userNickname}
+            onChange={handleChangeUserNickname}
+          />
+          <button
+            type="button"
+            className="btn-modify-profile btn-modify-user-nickname"
+          >Modify</button>
+        </div>
+        <div className="user-infos profile__user-password-container">
+          <label htmlFor="profile__user-password">New Password</label>
+          <input
+            id="profile__user-password"
+            type="password"
+            name={userPassword}
+            value={userPassword}
+            onChange={handleChangeUserPassword}
+          />
+        </div>
+        <div className="user-infos profile__user-confirm-pw-container">
+          <label htmlFor="profile__user-confirm-pw">Confirm Password</label>
+          <input
+            id="profile__user-password"
+            type="password"
+            name={confirmPw}
+            value={confirmPw}
+            onChange={handleChangeConfirmPw}
+          />
+          <button
+            type="button"
+            className="btn-modify-profile btn-modify-user-name"
+          >Modify</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default connect(null, {
+const mapStateToProps = (state) => ({
+  user: state.auth.user
+});
+
+export default connect(mapStateToProps, {
   uploadAvatar,
   deleteAvatar
 })(Profile);
