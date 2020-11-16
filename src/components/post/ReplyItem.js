bcd3bc6e9e153c6665259a4c2d7143e781fbac4f
jaehyeon48 <imjaehyeon48@gmail.com>
2020-11-16 14:21:05 +0900

    added showing reply's (edited) status

--- src/components/post/ReplyItem.js
+++ src/components/post/ReplyItem.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 
 import defaultAvatar from '../../img/default_avatar.png';
 
 require('dotenv').config();
 
-const ReplyItem = ({ reply }) => {
+const ReplyItem = ({
+  reply,
+  auth
+}) => {
   const signal = axios.CancelToken.source();
   const [replyUserNickname, setReplyUserNickname] = useState('');
   const [replyUserAvatar, setReplyUserAvatar] = useState('');
+  const [isOpenReplyEditor, setIsOpenReplyEditor] = useState(false);
 
   useEffect(() => {
     if (reply && reply.userId) {
@@ -44,10 +49,17 @@ const ReplyItem = ({ reply }) => {
         </div>
         <div className="reply__created-at">
           {reply.createdAt.replace('T', ' ').slice(0, 19)}
+          {reply && reply.isEdited === 1 && (
+            <span className="reply__is-edited">&#40;edited&#41;</span>
+          )}
         </div>
       </div>
     </div>
   );
 }
 
-export default ReplyItem;
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(ReplyItem);
